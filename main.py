3495b7a8e7dfcaebcd0fa1565ca9d6e9521ce2f4
divaniuss <dimasikemelka@gmail.com>
2024-12-17 19:41:14 +0200

    added def Division

--- main.py
+++ main.py
@@ -12,5 +12,10 @@ class Calculator:
     def Multiplication(self):
         return f"{self._a} * {self._b} = {self._a * self._b}"
 
+    def Division(self):
+        if self._b != 0:
+            return f"{self._a} / {self._b} = {self._a / self._b}"
+        return "Division on zero is impossible"
+
 c = Calculator(3,5)
 print(c.Addition())
