81f3f2e132b43c1938bed009b5182903eb8db7f8
divaniuss <dimasikemelka@gmail.com>
2024-12-17 19:37:11 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class Calculator:
+    def init(self,a,b):
+        self._a=a
+        self._b=b
+
+    def Addition(self):
+        return f"{self._a} + {self._b} = {self._a+self._b}"
+
+    def Subtraction(self):
+        return f"{self._a} - {self._b} = {self._a - self._b}"
+
+c = Calculator(3,5)
+print(c.Addition())
